7985ba3b102f1b0933a0335257c819845d32d33b
Mike Goren <mike@mortalwombat.net>
2023-02-01 08:45:28 -0600

    add routing and navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from '../Header'
 import TicketControl from '../TicketControl'
+import SignIn from "../SignIn";
 
 export default function App() {
   return (
     <div className='container'>
-      <Header />
-      <TicketControl />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/' element={<TicketControl />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import jackImage from "../../img/jack.jpg";
+import { Link, useLocation } from 'react-router-dom';
+import jackImage from '../../img/jack.jpg';
 import './Header.css';
 
 export default function Header() {
+  const url = useLocation().pathname;
+
   return (
     <section className='header'>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <span className="navbar-brand" href="/">Help Queue</span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to='/' className={`nav-link ${url === '/' && 'active'}`}>Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/sign-in' className={`nav-link ${url === '/sign-in' && 'active'}`}>Sign In</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <h1>Help Queue</h1>
       <img src={jackImage} height="250" alt="Jack Terricloth" />
       <hr/>
